bc6055fc5fa28a8aedfc8c96cdd4f5413b2cbe5b
MAYCON MORAES SILVA <maycon.silva@ojc.com.br>
2022-10-24 12:29:31 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const app = require('express')();
+const http = require('http')
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger_output.json')
+
+http.createServer(app).listen(3000, () => {
+    console.log('Server running on port 3000')
+})
+
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
+require('./endpoints.js')(app)
